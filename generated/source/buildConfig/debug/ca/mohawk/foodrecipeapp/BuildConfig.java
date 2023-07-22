a49f389fb095849fc610b614d75b922204759e5a
Aashika2001 <70160741+Aashika2001@users.noreply.github.com>
2023-07-22 22:02:28 +0530

    Add files via upload

---
+++ generated/source/buildConfig/debug/ca/mohawk/foodrecipeapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package ca.mohawk.foodrecipeapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "ca.mohawk.foodrecipeapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
